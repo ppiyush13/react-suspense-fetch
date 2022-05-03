1e85397c04ebfe80efdcc3c79a7fd53fab2589c2
Piyush Lodaya <ppiyush13@gmail.com>
2022-05-03 16:46:33 +0530

    Paralleize builds of main repo and demo repo

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/react-suspense-fetch/',
-  plugins: [react(), eslintPlugin()],
+  plugins: [react()],
 });
