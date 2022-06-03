7aa7e1cc2b90539db143deab3bee0b26d48a06df
Piyush Lodaya <ppiyush13@gmail.com>
2022-06-03 21:41:13 +0530

    Add comments

--- test-utils/waitForUpdate.ts
+++ test-utils/waitForUpdate.ts
@@ -1,12 +1,14 @@
 import { RenderHookResult, waitFor } from '@testing-library/react';
 
+/* https://github.com/testing-library/react-testing-library/pull/991 */
+
 export const waitForUpdate = async <Result, Props>(
   result: RenderHookResult<Result, Props>['result'],
 ) => {
-  /*current result */
+  /* current result */
   const currentResult = result.current;
 
-  /* wait until result.current is update  */
+  /* wait until result.current is updated */
   await waitFor(() => {
     expect(result.current).not.toBe(currentResult);
   });
